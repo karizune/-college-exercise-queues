9af98c92287ab351f759a09eb89c0bcd75a0f18f
Luan Chrystian Galan <luanchrystian2@gmail.com>
2020-08-13 19:53:47 -0300

    exercicio resolvido usando vetores e front-end para manuseio do aplicativo

---
+++ src/app/pagina/pagina.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { SistemaBancarioService } from './../services/sistema-bancario.service'
+
+@Component({
+  selector: 'app-pagina',
+  templateUrl: './pagina.component.html',
+  styleUrls: ['./pagina.component.css']
+})
+export class PaginaComponent implements OnInit {
+
+  senha1:string;
+  senha2:string;
+  senhaCliente:string;
+  constructor(
+    private service: SistemaBancarioService
+    ) { }
+    
+    ngOnInit(): void {
+    }
+    
+  adicionaCliente(tiposenha){
+    this.service.adicionaCliente(tiposenha);
+    this.senhaCliente = this.service.ultimodaFila();
+  }
+
+  chamadaCaixa(numeroCaixa:number){
+    if(numeroCaixa == 1){
+      this.senha1 = this.service.chamadaCaixa1();
+    }
+    else if(numeroCaixa == 2){
+      this.senha2 = this.service.chamadaCaixa2();
+    }
+  }
+
+  resetar(){
+    this.senhaCliente = "";
+    this.service.resetar();
+  }
+}
--- src/app/services/sistema-bancario.service.ts
+++ src/app/services/sistema-bancario.service.ts
@@ -5,5 +5,63 @@ import { Injectable } from '@angular/core';
 })
 export class SistemaBancarioService {
 
+  filaComum = [] ;
+  filaPrioritaria =[];
+  countComum: number = 0;
+  countPrioritario: number = 0;
+  senha:string;
+
   constructor() { }
+  
+  verificaFilas(){
+    return "Sem Clientes"
+  }
+
+  adicionaCliente(tipoSenha:number){
+    if(tipoSenha == 1){
+      this.countComum++;
+      this.senha = (`CXN-${(this.countComum).toString()}`);
+      this.filaComum.push(this.senha) ;
+    }
+    else if(tipoSenha == 2){
+      this.countPrioritario++;
+      this.senha = `CXP-${(this.countPrioritario + 1).toString()}`;
+      this.filaPrioritaria.push(this.senha);
+    } 
+  }
+
+  resetar(){
+    this.countComum = 0;
+    this.countPrioritario = 0;
+    this.filaComum = [];
+    this.filaPrioritaria = [];
+  }
+
+  ultimodaFila(){
+    return this.senha;
+  }
+
+  chamadaCaixa1(){
+    if(this.filaComum.length != 0){
+      return this.filaComum.shift();
+    }
+    else if(this.filaPrioritaria.length != 0){
+      return this.filaPrioritaria.shift();
+    }
+    else{
+      return "Sem clientes na fila"
+    }
+  }
+
+  chamadaCaixa2(){
+    if(this.filaPrioritaria.length != 0){
+      return this.filaPrioritaria.shift();
+    }
+    else if(this.filaComum.length != 0){
+      return this.filaComum.shift();
+    }
+    else{
+      return "Sem clientes na fila"
+    }
+  }
 }
